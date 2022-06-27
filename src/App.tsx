7d2858eccdb9220b27885a7953253ef25d4750df
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-06-27 16:40:36 -0300

    perf: add useCallback to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { Button } from "./components/Button";
 import { MovieCard } from "./components/MovieCard";
@@ -33,9 +33,9 @@ export function App() {
       });
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, []);
 
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
